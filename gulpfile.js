3fca0ca05d9d4ca93c758185b642630ebf404af4
Krishna Ashok <kashok@dealersocket.com>
2016-09-04 13:20:59 +0530

    upated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
 gulp.task('vet',function(){
-    return gulp
-        .src([
-            './components/**/*.js',
-            './directives/**/*.js',
-            './services/**/*.js',
-            './*.js'
-            ])
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish',{verbose:true}));
+	return gulp
+	.src([
+		'./components/**/*.js',
+		'./directives/**/*.js',
+		'./services/**/*.js',
+		'./*.js'
+		])
+	.pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish',{verbose:true}));
 });
